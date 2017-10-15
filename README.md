8d89ee40c868ba2a8a9489acebb27bf6ec15b181
Joel Stewart <jstewart.ca@gmail.com>
2017-10-14 19:05:41 -0700

    Added script and updated the README

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # poshbot
+
+===============================================================
+Introduction
+===============================================================
+This repository includes a Ruby script that leverages Selenium 
+to login to Poshmark, navigate to a specific closet, share the
+entire closet with the logged in user's followers. 
+
+Sharing is limited to available items only and are shared in a 
+manner that preserves the order of the closet when completed 
+successfully. 
+
+===============================================================
+Requirements
+===============================================================
+In order to utilize the script, the following prerequisities 
+must be met: 
+
+1. Ruby must be installed (minimum version 2.0)
+2. Google Chrome Browser (minimum version 55.0)
+3. ChromeDriver - WebDriver for Chrome (version 2.33)
+
+===============================================================
+Usage
+===============================================================
+1. Edit variables in the SeleniumPosh.rb file
+    * USER - This is the username that is used for login
+    * PASSWORD - This is the password required for login
+    * CLOSET - This is the username of the closet to share. 
+               This is commonly the same value as the USER. 
+
+2. Run the script: `ruby SeleniumPosh.rb`
---
+++ SeleniumPosh.rb
@@ -0,0 +1,55 @@
+require 'rubygems'
+require 'selenium-webdriver'
+
+USER = ""
+PASSWORD = ""
+CLOSET = ""
+
+driver = Selenium::WebDriver.for :chrome
+wait = Selenium::WebDriver::Wait.new(:timeout => 60)
+
+# Login
+driver.get "https://poshmark.com/login"
+driver.find_element(:id, "login_form_username_email").send_keys USER
+driver.find_element(:id, "login_form_password").send_keys PASSWORD
+driver.find_element(:id, "login_form_password").submit
+wait.until { driver.title.start_with? "Feed" } 
+
+# Load Full Closet
+driver.get "https://poshmark.com/closet/" + CLOSET + "?availability=available"
+loader = driver.find_element(:id, "load-more")
+old_shares = driver.find_elements(:class, "share")
+shares = []
+
+while (old_shares.size != shares.size) do 
+  old_shares = shares
+  driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+  if loader.displayed? 
+    wait.until { !driver.find_element(:id, "load-more").displayed? }
+  end
+  sleep 0.5
+  shares = driver.find_elements(:class, "share")
+#  puts "There are #{shares.size} objects to share"
+end
+
+# Share
+shares.reverse.each_with_index do |i, index| 
+  spot = i.location 
+  driver.execute_script("window.scrollTo(#{spot.x}, #{spot.y - 400})")
+  i.click
+  driver.find_element(:class, "pm-followers-share-link").click
+  wait.until { driver.find_element(:class, "flash-con").displayed? }
+#  puts "Shared #{index+1}/#{shares.size}: #{i.attribute("data-pa-attr-listing_id")}"
+  if (index+1)%10 == 0
+    puts "Shared #{index+1}/#{shares.size}: #{i.attribute("data-pa-attr-listing_id")}"
+  end
+  wait.until { !driver.find_element(:class, "flash-con").displayed? }
+end
+
+puts "Successfully shared #{shares.size} items with your followers"
+
+# Logout
+driver.get "https://poshmark.com/logout"
+wait.until { driver.title.start_with? "Poshmark" }
+
+driver.quit
